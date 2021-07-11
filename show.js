2530cd63a69ccd03f946ab1b3db8c76f3971ccd2
Dinesh Jangir <87252360+digitalduniyasite@users.noreply.github.com>
2021-07-11 11:19:22 +0530

    Update show.js

--- show.js
+++ show.js
@@ -12,69 +12,65 @@ let nextpage = document.getElementById('nextpage');
 
 let curpage = document.getElementById('curpage');
 
+back.innerHTML = "";
+backpage.innerHTML = "";
+back.value = "";
 
-back.innerHTML="";
-backpage.innerHTML="";
-back.value=""; 
- 
-nextpage.innerHTML=""; 
-next.innerHTML=""; 
+nextpage.innerHTML = "";
+next.innerHTML = "";
 
-
-
-let error  = document.getElementById("error");
+let error = document.getElementById("error");
 
 let modal_close = document.getElementsByClassName('close');
- 
+
 let modal = document.getElementsByClassName('modal');
- 
+
 let newuser = document.getElementsByClassName('new1');
 
-let tr = "" 
+let tr = ""
 
 let tdd = ""
 
-let lmt = "" 
-
-for(let i = 0; i < modal_close.length ; i++){
+let lmt = ""
 
 // close start
 
+for (let i = 0; i < modal_close.length; i++) {
 
-modal_close[i].addEventListener("click" , ()=>{
+  modal_close[i].addEventListener("click", () => {
 
-for(let j = 0; j < modal_close.length ; j++){
-modal[j].classList.remove('block')
-modal[j].classList.add('none')
-}
- })
+    for (let j = 0; j < modal_close.length; j++) {
+      modal[j].classList.remove('block')
+      modal[j].classList.add('none')
+    }
+  })
 }
 
 // close exit
 // show modal start
 
 
-let showfunction = (cname)=>{
-cname.classList.remove('none')
-cname.classList.add('block')
+let showfunction = (cname) => {
+  cname.classList.remove('none')
+  cname.classList.add('block')
 }
 
 // show modal end
 
 var valshow = 15;
-function showrec(valuee){ 
-valshow = valuee;
-showfunction(users);
+function showrec(valuee) {
+  valshow = valuee;
+  showfunction(users);
 
-tdd = "";
- 
-fetch("https://gorest.co.in/public-api/users?id="+valshow)
-.then((data)=>{
-    return data.json()
-})
-.then((dataa)=>{ 
+  tdd = "";
 
-tdd = `<td>${dataa.data[0].id}</td>
+  fetch("https://gorest.co.in/public-api/users?id=" + valshow)
+    .then((data) => {
+      return data.json()
+    })
+    .then((dataa) => {
+
+      tdd = `<td>${dataa.data[0].id}</td>
     <td>${dataa.data[0].name} </td>
     
     <td>${dataa.data[0].email} </td>
@@ -84,89 +80,90 @@ tdd = `<td>${dataa.data[0].id}</td>
     <td>${dataa.data[0].status} </td>
  `;
 
-single_data.innerHTML = tdd;
-})
-.catch((error)=>{
-    console.log(error);
-})
+      single_data.innerHTML = tdd;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 }
- 
+
 // load data function
 
-let pageno = 1; 
+let pageno = 1;
 
-next.addEventListener("click",()=>{pageno = next.value;
-    loaddata()
+next.addEventListener("click", () => {
+  pageno = next.value;
+  loaddata()
 })
 
 
-back.addEventListener("click",()=>{pageno = 1;
-    loaddata()
+back.addEventListener("click", () => {
+  pageno = 1;
+  loaddata()
 })
 
 
-nextpage.addEventListener("click",()=>{pageno = pageno+1;
-    loaddata()
+nextpage.addEventListener("click", () => {
+  pageno = pageno + 1;
+  loaddata()
 })
 
 
-backpage.addEventListener("click",()=>{pageno = pageno-1;
-    loaddata()
+backpage.addEventListener("click", () => {
+  pageno = pageno - 1;
+  loaddata()
 })
- 
- 
-
-loaddata() ;
-function loaddata(){
-
-let url = `https://gorest.co.in/public-api/users?page=${pageno}`;
- 
-fetch(url)
-.then((data)=>{
-	return data.json()
-})
-.then((dataa)=>{
-
-let limit = dataa.meta.pagination.limit;
-
-let page = dataa.meta.pagination.page;
-
-let tpages = dataa.meta.pagination.pages;
-
-let totalrecords = dataa.meta.pagination.total;
-
-plimit = (page-1)*limit;
-poffset = plimit+limit;
-
-curpage.innerHTML=page;
-back.innerHTML="≪";
-backpage.innerHTML=page-1; 
-nextpage.innerHTML=page+1; 
-next.innerHTML="≫"; 
-next.value=tpages; 
- 
-dataas.innerHTML = "";
-
-if(page-1 == 0){
-   back.className += " none"; 
-   backpage.className += " none"; 
- }else if(page-1 > 0){
-   back.className  = ""; 
-   backpage.className  = "";
-}
-
-if(page == tpages){
-   next.className += " none"; 
-   nextpage.className += " none";
-}else{
-    next.className  = ""; 
-   nextpage.className  = "";
-} 
 
 
-tr = "";
-for(let i in dataa.data){
-tr += `<tr> <td>${dataa.data[i].id}</td>
+
+loaddata();
+function loaddata() {
+
+  let url = `https://gorest.co.in/public-api/users?page=${pageno}`;
+
+  fetch(url)
+    .then((data) => {
+      return data.json()
+    })
+    .then((dataa) => {
+
+      let limit = dataa.meta.pagination.limit;
+
+      let page = dataa.meta.pagination.page;
+
+      let tpages = dataa.meta.pagination.pages;
+
+      let totalrecords = dataa.meta.pagination.total;
+
+      curpage.innerHTML = page;
+      back.innerHTML = "≪";
+      backpage.innerHTML = page - 1;
+      nextpage.innerHTML = page + 1;
+      next.innerHTML = "≫";
+      next.value = tpages;
+
+      dataas.innerHTML = "";
+
+      if (page - 1 == 0) {
+        back.className += " none";
+        backpage.className += " none";
+      } else if (page - 1 > 0) {
+        back.className = "";
+        backpage.className = "";
+      }
+
+      if (page == tpages) {
+        next.className += " none";
+        nextpage.className += " none";
+      } else {
+        next.className = "";
+        nextpage.className = "";
+      }
+
+
+      tr = "";
+      for (let i in dataa.data) {
+        tr += `<tr> <td>${dataa.data[i].id}</td>
     <td>${dataa.data[i].name} </td>
     <td>
 <span class="span1"  onclick="showrec(${dataa.data[i].id})" id="show${dataa.data[i].id}">show</span>
@@ -174,42 +171,42 @@ tr += `<tr> <td>${dataa.data[i].id}</td>
 <span class="span1"  onclick="deleterec(${dataa.data[i].id})">delete</span>
     </td>
 <tr>`;
+      }
+
+      dataas.innerHTML = tr;
+
+
+    })
+
+
+    .catch((error) => {
+
+      error.innerHTML = "error Found";
+      error.className = 'block';
+    })
 }
 
-dataas.innerHTML = tr;
- 
- 
-})
-
-
-.catch((error)=>{
-
-error.innerHTML = "error Found";
-error.className = 'block';
-})
-} 
 
 
 
 
- 
 // update data code
 
 var updatedata = document.getElementById("updateee");
 
-function editrec(valuee){ 
-valshow = valuee;
-showfunction(editusers);
+function editrec(valuee) {
+  valshow = valuee;
+  showfunction(editusers);
 
-tdd = "";
- 
-fetch("https://gorest.co.in/public-api/users?id="+valshow)
-.then((data)=>{
-    return data.json()
-})
-.then((dataa)=>{
+  tdd = "";
 
-tdd = ` 
+  fetch("https://gorest.co.in/public-api/users?id=" + valshow)
+    .then((data) => {
+      return data.json()
+    })
+    .then((dataa) => {
+
+      tdd = ` 
 <input type="text" id="id${dataa.data[0].id}" placeholder="User id" value="${dataa.data[0].id}">
 
 <input type="text" id="n${dataa.data[0].id}" placeholder="name" value="${dataa.data[0].name}">
@@ -223,28 +220,28 @@ tdd = `
 <input type="submit" id="su${dataa.data[0].id}" value="submit update" onclick="update_data(${dataa.data[0].id});">
 `
 
-updatedata.innerHTML = tdd;
-})
-.catch((error)=>{
-    console.log(error);
-})
+      updatedata.innerHTML = tdd;
+    })
+    .catch((error) => {
+      console.log(error);
+    })
 }
 
-function update_data(idupdate){
-console.log(idupdate);
+function update_data(idupdate) {
+  console.log(idupdate);
 
 
-var id = document.getElementById(`id${idupdate}`);
+  var id = document.getElementById(`id${idupdate}`);
 
-var name = document.getElementById(`n${idupdate}`);
+  var name = document.getElementById(`n${idupdate}`);
 
-var email = document.getElementById(`e${idupdate}`);
+  var email = document.getElementById(`e${idupdate}`);
 
-var gender = document.getElementById(`g${idupdate}`);
+  var gender = document.getElementById(`g${idupdate}`);
 
-var status = document.getElementById(`s${idupdate}`);
+  var status = document.getElementById(`s${idupdate}`);
 
-var dataaa = JSON.stringify({
+  var dataaa = JSON.stringify({
     id: id.value,
     name: name.value,
     email: email.value,
@@ -252,40 +249,40 @@ var dataaa = JSON.stringify({
     status: status.value
   })
 
-console.log(dataaa)
-// var urll = `https://gorest.co.in/public/v1/users?id=${idupdate}`; 
+  console.log(dataaa)
+  // var urll = `https://gorest.co.in/public/v1/users?id=${idupdate}`; 
 
- var urll = `https://gorest.co.in/public-api/users/${idupdate}`;
+  var urll = `https://gorest.co.in/public-api/users/${idupdate}`;
 
-fetch(urll , {
-  method: 'PUT',
-  body: dataaa,
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) =>{ 
-return response.json()
-console.log(response.json());
-})
-  
-  .then((json) =>{ 
-  alert("finel update result : " + json.data.message);
-
-for(let j = 0; j < modal_close.length ; j++){
-modal[j].classList.remove('block')
-modal[j].classList.add('none')
-}
+  fetch(urll, {
+    method: 'PUT',
+    body: dataaa,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
-  .catch((error)=>{
-alert('update error')
+    .then((response) => {
+      return response.json()
+      console.log(response.json());
+    })
 
-for(let j = 0; j < modal_close.length ; j++){
-modal[j].classList.remove('block')
-modal[j].classList.add('none')
-}
+    .then((json) => {
+      alert("finel update result : " + json.data.message);
 
-  })
+      for (let j = 0; j < modal_close.length; j++) {
+        modal[j].classList.remove('block')
+        modal[j].classList.add('none')
+      }
+    })
+    .catch((error) => {
+      alert('update error')
+
+      for (let j = 0; j < modal_close.length; j++) {
+        modal[j].classList.remove('block')
+        modal[j].classList.add('none')
+      }
+
+    })
 
 
 }
@@ -297,19 +294,19 @@ modal[j].classList.add('none')
 // save data 
 
 
-function davedata(){
+function davedata() {
 
-var id = document.getElementById('id');
+  var id = document.getElementById('id');
 
-var name = document.getElementById('name');
+  var name = document.getElementById('name');
 
-var email = document.getElementById('email');
+  var email = document.getElementById('email');
 
-var gender = document.getElementById('gender');
+  var gender = document.getElementById('gender');
 
-var status = document.getElementById('status');
+  var status = document.getElementById('status');
 
-var dataaa = JSON.stringify({
+  var dataaa = JSON.stringify({
     id: id.value,
     name: name.value,
     email: email.value,
@@ -317,31 +314,31 @@ var dataaa = JSON.stringify({
     status: status.value
   })
 
-fetch('https://gorest.co.in/public-api/users', {
-  method: 'POST',
-  body: dataaa,
-  headers: {
-    'Content-type': 'application/json; charset=UTF-8',
-  },
-})
-  .then((response) => response.json())
-  .then((json) =>{ 
-
-    alert("finel Insert result : " + json.data.message);
- ;
-
-var form = document.getElementById("form");
-
-for(let j = 0; j < modal_close.length ; j++){
-modal[j].classList.remove('block')
-modal[j].classList.add('none')
-}
-form.reset();
-
+  fetch('https://gorest.co.in/public-api/users', {
+    method: 'POST',
+    body: dataaa,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
   })
-    .catch((error)=>{
-console.log(error)
-  })
+    .then((response) => response.json())
+    .then((json) => {
+
+      alert("finel Insert result : " + json.data.message);
+      ;
+
+      var form = document.getElementById("form");
+
+      for (let j = 0; j < modal_close.length; j++) {
+        modal[j].classList.remove('block')
+        modal[j].classList.add('none')
+      }
+      form.reset();
+
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 
 
 }
@@ -352,20 +349,20 @@ console.log(error)
 // delete Data
 
 
-function deleterec(iddd){
+function deleterec(iddd) {
 
-// var urll = `https://gorest.co.in/public/v1/users?id=${iddd}`; 
+  // var urll = `https://gorest.co.in/public/v1/users?id=${iddd}`; 
 
-var urll = `https://gorest.co.in/public-api/users/${iddd}`; 
+  var urll = `https://gorest.co.in/public-api/users/${iddd}`;
 
 
-fetch(urll , {
-  method: 'DELETE',
-}).then((asdf)=>{
-   return asdf.json();
-})
-.then((asdf)=>{
-    alert("Delete Message : "+ asdf.data.message);
-})
-;
+  fetch(urll, {
+    method: 'DELETE',
+  }).then((asdf) => {
+    return asdf.json();
+  })
+    .then((asdf) => {
+      alert("Delete Message : " + asdf.data.message);
+    })
+    ;
 }
